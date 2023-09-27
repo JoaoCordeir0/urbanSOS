3af0a22d783b545bb587c41e5442498d94ad0198
joao.cordeiro <joaocordeiro2134@gmail.com>
2023-09-27 12:31:23 -0300

    Implementa bottomnavigator com fragments

--- app/src/main/java/br/com/urbansos/Functions.java
+++
@@ -1,6 +0,0 @@
-package br.com.urbansos;
-
-public class Functions
-{
-
-}
--- app/src/main/java/br/com/urbansos/Main.java
+++ app/src/main/java/br/com/urbansos/Main.java
@@ -1,15 +1,21 @@
 package br.com.urbansos;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import br.com.urbansos.fragments.CameraFragment;
+import br.com.urbansos.fragments.HomeFragment;
+import br.com.urbansos.fragments.SettingsFragment;
 
 public class Main extends AppCompatActivity {
 
@@ -28,7 +34,6 @@ public class Main extends AppCompatActivity {
                     setContentView(R.layout.login);
                 }
             }, 3000);
-
         }
         catch (Exception ex) {}
     }
@@ -36,6 +41,41 @@ public class Main extends AppCompatActivity {
     public void screenForgotPassword(View view) { setContentView(R.layout.forgotpassword); }
     public void screenLogin(View view) { setContentView(R.layout.login); }
     public void screenSignup(View view) { setContentView(R.layout.signup); }
-    public void screenHome(View view) { setContentView(R.layout.home); }
-    public void screenSettings(View view) { setContentView(R.layout.settings); }
+    public void screenMain(View view)
+    {
+        setContentView(R.layout.main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        setFragment(new HomeFragment());
+        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.fragment_home)
+                {
+                    setFragment(new HomeFragment());
+                    return true;
+                }
+                else if (id == R.id.fragment_camera)
+                {
+                    setFragment(new CameraFragment());
+                    return true;
+                }
+                else if (id == R.id.fragment_settings)
+                {
+                    setFragment(new SettingsFragment());
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame_layout, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
 }
